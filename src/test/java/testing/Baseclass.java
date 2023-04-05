407f376539cbbe457fcdee830a07908be0e2706d
Anusuyaa12 <anusha121995@gmail.com>
2023-04-05 22:20:17 +0530

    message

---
+++ src/test/java/testing/Baseclass.java
@@ -0,0 +1,19 @@
+package testing;
+
+public class Baseclass {
+
+private void m1() {
+System.out.println("m1");
+}
+	
+private void m2() {
+System.out.println("m2");
+}
+
+private void m3() {
+System.out.println("m3");
+}
+
+	}
+
+
